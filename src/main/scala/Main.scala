f05d943224fe9b2faba778f538907fe312c55103
haretaro <frontend.1000@gmail.com>
2015-12-19 21:53:26 +0900

    マウスの座標取得

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,23 @@
-import java.awt.Robot
+import java.awt._
 import java.awt.event.InputEvent
 
 object Main extends App{
   val robot = new Robot()
   val leftclick = InputEvent.BUTTON1_DOWN_MASK
-  robot.mouseMove(100,100)
-  def go:Unit = {
-    robot.mousePress(leftclick)
-    robot.mouseRelease(leftclick)
-    go
+
+  robot.setAutoDelay(50)
+  robot.mouseMove(200,300)
+
+  def go(i:Int):Unit = i match {
+    case i if i < 100000 =>{
+      MouseInfo.getPointerInfo 
+      //robot.mousePress(leftclick)
+      //robot.mouseRelease(leftclick)
+      println(MouseInfo.getPointerInfo.getLocation.x)
+      println(MouseInfo.getPointerInfo.getLocation.y)
+      go(i+1)
+    }
+    case _ => ()
   }
-  go
+  go(0)
 }
