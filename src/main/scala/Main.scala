c8042331cb3475df4dd499dcd02bd1543ebbcefd
haretaro <frontend.1000@gmail.com>
2015-12-20 23:31:24 +0900

    add Vector2 class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,22 @@
-import java.awt._
+import java.awt.{Robot, MouseInfo}
 import java.awt.event.InputEvent
 
 object Main extends App{
   val robot = new Robot()
   val leftclick = InputEvent.BUTTON1_DOWN_MASK
-
   robot.setAutoDelay(50)
   robot.mouseMove(200,300)
 
-  def go(i:Int):Unit = i match {
-    case i if i < 100000 =>{
-      MouseInfo.getPointerInfo 
+  def go(i:Int, history:List[Vector2]):Unit = i match {
+    case i if i < 10000 =>{
       //robot.mousePress(leftclick)
       //robot.mouseRelease(leftclick)
-      println(MouseInfo.getPointerInfo.getLocation.x)
-      println(MouseInfo.getPointerInfo.getLocation.y)
-      go(i+1)
+      val x = MouseInfo.getPointerInfo.getLocation.x
+      val y = MouseInfo.getPointerInfo.getLocation.y
+      println((x,y))
+      go(i+1, Vector2(x,y) :: history)
     }
     case _ => ()
   }
-  go(0)
+  go(0, List.fill(10)(Vector2(0,0)))
 }
---
+++ src/main/scala/Vector2.scala
@@ -0,0 +1,5 @@
+case class Vector2(x:Double, y:Double){
+  def +(that: Vector2):Vector2 = Vector2(x + that.x, y + that.y)
+
+  def angle: Double = math.cos(x) + math.sin(x)
+}
