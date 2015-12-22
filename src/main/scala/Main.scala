55d39eabb9568978e3ca5763ef17f2970d1c7126
haretaro <frontend.1000@gmail.com>
2015-12-22 23:28:50 +0900

    diff関数を作ったけどまだ動いてない

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,14 +7,17 @@ object Main extends App{
   robot.setAutoDelay(50)
   robot.mouseMove(200,300)
 
+  def diff[A](as: Seq[A]): Seq[A] = as.zip(as.drop(1)).map((a1:A, a2:A) => a1 - a2)
+
   def go(i:Int, history:Seq[Vector2]):Unit = i match {
     case i if i < 10000 =>{
       //robot.mousePress(leftclick)
       //robot.mouseRelease(leftclick)
       val x = MouseInfo.getPointerInfo.getLocation.x
       val y = MouseInfo.getPointerInfo.getLocation.y
-      println((x,y))
-      go(i+1, Vector2(x,y) +: history)
+      println(diff(history))
+      //println(history.foldLeft(0d)((acc, v) => (acc + v.angle)))
+      go(i+1, Vector2(x,y) +: history.take(9))
     }
     case _ => ()
   }
